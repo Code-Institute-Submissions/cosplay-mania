e559053ca4f8f2376f9266ec41d82fb25d5be987
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-20 17:17:23 +0000

    setup routes to initial pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="50" /></Navbar.Brand>
+        <NavLink to="/">
+            <Navbar.Brand href="#home"><img src={logo} alt="logo" height="50" /></Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link><i class="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-user"></i>Sign up</Nav.Link>
+            <NavLink to="/" exact className={styles.NavLink} activeClassName={styles.Active}>
+                <i class="fa-solid fa-house"></i>Home
+            </NavLink>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}> 
+                <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}> 
+                <i class="fa-solid fa-user"></i>Sign up
+            </NavLink>
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
