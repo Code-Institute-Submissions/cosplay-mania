7508da1394bd1e2465be34fe4bad8e7c247f3484
Lapratomo24 <lapratomo24@yahoo.com>
2022-11-20 16:50:07 +0000

    update spacing between navbar and  main content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import styles from "./App.module.css";
 import NavBar from './components/Navbar';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Welcome to Animeniacs!</h1>
+      </Container>
     </div>
   );
 }
